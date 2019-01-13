5d73bd6118d309cec3deca485fa7965586527fa5
Xiaohan Zhang <zxh123123@gmail.com>
2019-01-12 22:20:44 -0800

    edit convertor.py to find ouwn image AU

--- converter.py
+++ converter.py
@@ -46,10 +46,15 @@ class feedFoward:
 
 
 def main():
-    path = './checkpoints/original_model/net_epoch_30_id_G.pth'
+    path = './checkpoints/refined_model/net_epoch_30_id_G.pth'
     convertor = feedFoward(path)
     # dummy_input = np.random.randint(255, size=(128, 128, 3)).astype(np.uint8)
-    real_face_raw = cv2.imread('./sample_dataset/imgs/000312.jpg')
+    name = '202204'
+    real_face_raw = cv2.imread('./sample_dataset/imgs/'+name+'.png')
+
+    # real_face_raw = real_face_raw[15:203, 0:178]
+    # real_face_raw = cv2.resize(real_face_raw,(128,128))
+
     real_face = cv2.cvtColor(real_face_raw, cv2.COLOR_BGR2RGB)
     expressions = np.ndarray((10,17), dtype = np.float)
 
@@ -63,9 +68,17 @@ def main():
     expressions[2] = np.array([0, 0, 5, 0, 0, 5, 0, 1.5, 0, 0, 0, 2.5, 0, 1.5, 0, 0, 0], dtype = np.float) # angry
     expressions[3] = np.array([5, 5, 0, 3.3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 5, 0, 0], dtype = np.float) # surprise
     '''
+    a = np.array([0.25, 0.11, 0.2 , 0.16, 1.92, 1.03, 0.3 , 2.15, 2.88, 1.61, 0.03, 0.09, 0.16, 0.11, 2.25, 0.37, 0.05], dtype = np.float)
+    # b = np.array([0.69, 0, 0.01 , 0, 0.19, 1.02, 0 , 0, 1.25, 1.41, 0, 0, 0.59, 0.05, 0.9, 0, 0.33], dtype = np.float)
+    b = conds[name]/10
+    c = (b - a)/9
+    print(c)
+    print('-------------------------')
     for i in range(10):
-        expressions[i] =  random.choice(list(conds.values())) + np.random.uniform(-0.1, 0.1, 17)
-        print(expressions[0])
+        # expressions[i] = np.array([0.25, 0.11, 0.2 , 0.16, 1.92, 1.03, 0.3 , 2.15, 2.88, 1.61, 0.03, 0.09, 0.16, 0.11, 2.25, 0.37, 0.05], dtype = np.float) + np.random.uniform(-0.1, 0.1, 17)
+        # expressions[i] = np.array([0.69, 0, 0.01 , 0, 0.19, 1.02, 0 , 0, 1.25, 1.41, 0, 0, 0.59, 0.05, 0.9, 0, 0.33], dtype = np.float) + np.random.uniform(-0.1, 0.1, 17)
+        expressions[i] = c * i + a
+        print(expressions[i])
 
     # origin_expression = np.array([0, 0, 0.46, 0, 0, 0, 0.03, 0, 0, 0.59, 0.02, 0.22, 0, 0.21, 0, 0, 0], dtype = np.float)
 
